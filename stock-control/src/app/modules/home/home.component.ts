08bca65ea477003c53ae93e3d9695d36399d3e17
Guilherme H Perosso <guilhermeperosso53@gmail.com>
2025-12-26 10:32:55 -0300

    Estilo e interação básica dos formulários

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmailValidator, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,24 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   loginCard = true;
 
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  signpForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  onSubmitLoginForm(): void {
+    console.log("Dados do Formulário de Login: ", this.loginForm.value)
+  }
+
+  onSubmitSignUpForm(): void {
+    console.log("Dados do Formulário de Criação de Conta: ", this.signpForm.value)
+  }
 }
